4d6e9bb83935fdf5ebe496b0390f297cd7b949d1
Orbit419 <rkrylov97@gmail.com>
2018-11-26 12:54:57 +0200

    Test

--- src/HomeWork/Lesson_1/Romb.java
+++ src/HomeWork/Lesson_1/Romb.java
@@ -4,29 +4,50 @@ import java.util.Scanner;
 
 public class Romb {
 
-    static int size;
-    Scanner scanner = new Scanner(System.in);
-    String[][] romb = new String[size][size];
 
     public static void main(String[] arg){
-
-
-
+        int size = setSize();
+        String[][] romb = makeRomb(size);
+        printRomb(romb);
     }
-//    String[][] makeRomb(String[][] romb){
-//        for(int x = 0; x<size; x++){
-//            for(int y =
-//        }
-//    }
+    static String[][] makeRomb(int size){
+        String[][] matrix = new String[size][size];
+        int center = size/2;
+        int countStars = 0;
 
-    void setSize(){
+        for(int x = 0; x<size; x++){
+            for(int y = 0; y<size; y++){
+                if(y < center-countStars || y > center+countStars)
+                    matrix[x][y] = " ";
+                else
+                    matrix[x][y] = "*";
+            }
+            if(x<center)
+                countStars++;
+            else
+                countStars--;
+        }
+        return matrix;
+    }
+
+    static int setSize(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter size of romb(odd(непарное) number):");
         int value = scanner.nextInt();
-        if(value%2 != 0) size = value;
+        if(value%2 != 0) return value;
         else {
             System.out.println("Odd number!");
             value = scanner.nextInt();
-            size = value;
+            return value;
+        }
+    }
+
+    static void printRomb(String[][] matrix){
+        for(int x = 0; x < matrix.length; x++){
+            for(int y = 0; y < matrix.length; y++){
+                System.out.print(matrix[x][y]+" ");
+            }
+            System.out.println("\t");
         }
     }
 }
